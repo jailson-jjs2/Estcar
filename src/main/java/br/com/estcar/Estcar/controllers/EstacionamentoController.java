90330224e5a660458239457ef09d8d3d96cbccae
JailsonJJS2 <jjs2@hotmail.com>
2023-09-18 08:44:11 -0300

    Concluido modo alt vaga

--- src/main/java/br/com/estcar/Estcar/controllers/EstacionamentoController.java
+++ src/main/java/br/com/estcar/Estcar/controllers/EstacionamentoController.java
@@ -1,7 +1,6 @@
 package br.com.estcar.Estcar.controllers;
 
 import java.io.UnsupportedEncodingException;
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,6 +21,7 @@ import br.com.estcar.Estcar.models.VagaEstacionamento;
 import br.com.estcar.Estcar.repositorio.AdministradoresRepo;
 import br.com.estcar.Estcar.repositorio.VagaEstacionamentoRepo;
 import br.com.estcar.Estcar.servico.CookieService;
+import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
 @Controller
 public class EstacionamentoController {
@@ -113,5 +114,63 @@ public class EstacionamentoController {
 	    }
 	    
 	    return "redirect:/estacionamento";
-	}				
+	}		
+	
+	
+	
+	@GetMapping("/estacionamento/{id}")
+	public String buscar(@PathVariable int id, Model model, HttpServletRequest request) throws UnsupportedEncodingException {
+	    java.util.Optional<VagaEstacionamento> vaga = vagaRepo.findById(id);
+	    if (vaga.isPresent()) {
+	        model.addAttribute("vagaEstacionamento", vaga.get());
+
+	        // Obtenha o ID do administrador a partir do cookie
+	        String adminIdCookie = CookieService.getCookie(request, "usuarioId");
+	        if (adminIdCookie != null) {
+	            int administradorId = Integer.parseInt(adminIdCookie);
+	            Administrador administradorLogado = adminRepo.findById(administradorId).orElse(null);
+	            if (administradorLogado != null) {
+	                // Obtenha a quantidade máxima de vagas permitidas pelo administrador
+	                int quantidadeMaximaVagas = administradorLogado.getQuant_vaga();
+
+	                // Crie uma lista de todas as vagas disponíveis
+	                List<Integer> todasVagasDisponiveis = new ArrayList<>();
+	                for (int i = 1; i <= quantidadeMaximaVagas; i++) {
+	                    todasVagasDisponiveis.add(i);
+	                }
+
+	                // Obtenha as vagas já criadas pelo administrador
+	                List<VagaEstacionamento> vagasCriadas = administradorLogado.getVagasEstacionamento();
+	                List<Integer> vagasCriadasList = vagasCriadas.stream()
+	                        .map(v -> Integer.parseInt(v.getNumVaga()))
+	                        .collect(Collectors.toList());
+
+	                // Remova a vaga atual das vagas disponíveis (para que ela possa ser selecionada novamente)
+	                VagaEstacionamento vagaAtual = vaga.get();
+	                todasVagasDisponiveis.add(Integer.parseInt(vagaAtual.getNumVaga()));
+
+	                // Remova as vagas já criadas das vagas disponíveis
+	                todasVagasDisponiveis.removeAll(vagasCriadasList);
+
+	                // Passe as vagas disponíveis para o modelo
+	                model.addAttribute("vagasDisponiveis", todasVagasDisponiveis);
+
+	                return "estacionamento/editar";
+	            }
+	        }
+	    }
+	    return "redirect:/estacionamento";
+	}
+
+    @PostMapping("/estacionamento/{id}/atualizar")
+    public String atualizar(@PathVariable int id, VagaEstacionamento vaga) {
+        java.util.Optional<VagaEstacionamento> vagaExistente = vagaRepo.findById(id);
+        if (vagaExistente.isPresent()) {
+            VagaEstacionamento vagaAtual = vagaExistente.get();
+            vagaAtual.setNumVaga(vaga.getNumVaga());
+            vagaAtual.setPlacaCarro(vaga.getPlacaCarro());
+            vagaRepo.save(vagaAtual);
+        }
+        return "redirect:/estacionamento";
+    }
 }
