d864d450f5a37a22a892902849903d8b0edc4709
JailsonJJS2 <jjs2@hotmail.com>
2023-09-19 14:58:43 -0300

    Concluido primeira parte do financeiro

--- src/main/java/br/com/estcar/Estcar/controllers/RegistrosVagaController.java
+++ src/main/java/br/com/estcar/Estcar/controllers/RegistrosVagaController.java
@@ -1,14 +1,20 @@
 package br.com.estcar.Estcar.controllers;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.estcar.Estcar.models.Administrador;
 import br.com.estcar.Estcar.models.RegistrosVaga;
+import br.com.estcar.Estcar.repositorio.AdministradoresRepo;
 import br.com.estcar.Estcar.repositorio.RegistrosVagaRepo;
+import br.com.estcar.Estcar.servico.CookieService;
 
 @Controller
 public class RegistrosVagaController {
@@ -16,17 +22,35 @@ public class RegistrosVagaController {
 	@Autowired
 	private RegistrosVagaRepo repo;
 	
-	@GetMapping("/financeiro")
-	public String index(Model model) {
-		List<RegistrosVaga> registros = (List<RegistrosVaga>)repo.findAll();
-		
-	    // Formate a data antes de passá-la para o Thymeleaf
-	    for (RegistrosVaga registro : registros) {
-	        registro.setDataLiberecaoFormatada(registro.getDataLiberacaoFormatada());
-	    }
-	    
-		model.addAttribute("registros", registros);
-		
-	return "financeiro/index";
-	}
+	@Autowired
+    private AdministradoresRepo adminRepo;
+
+    @GetMapping("/financeiro")
+    public String index(Model model, HttpServletRequest request) throws UnsupportedEncodingException {
+        // Obter o ID do administrador a partir do cookie
+        String adminIdCookie = CookieService.getCookie(request, "usuarioId");
+        if (adminIdCookie != null) {
+            int administradorId = Integer.parseInt(adminIdCookie);
+            Administrador administradorLogado = adminRepo.findById(administradorId).orElse(null);
+            if (administradorLogado != null) {
+                // Consultar os registros financeiros associados ao administrador logado
+                List<RegistrosVaga> registros = repo.findByAdministrador(administradorLogado);
+
+                // Formate a data antes de passá-la para o Thymeleaf
+                for (RegistrosVaga registro : registros) {
+                    registro.setDataLiberecaoFormatada(registro.getDataLiberacaoFormatada());
+                }
+
+                // Calcular a soma dos valores pagos
+                double valorTotalAPagar = registros.stream()
+                        .mapToDouble(RegistrosVaga::getValorPago)
+                        .sum();
+
+                model.addAttribute("registros", registros);
+                model.addAttribute("valorTotalAPagar", valorTotalAPagar);
+            }
+        }
+        return "financeiro/index";
+    }
+	
 }
--- src/main/java/br/com/estcar/Estcar/repositorio/RegistrosVagaRepo.java
+++ src/main/java/br/com/estcar/Estcar/repositorio/RegistrosVagaRepo.java
@@ -1,13 +1,22 @@
 package br.com.estcar.Estcar.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import br.com.estcar.Estcar.models.Administrador;
 import br.com.estcar.Estcar.models.RegistrosVaga;
 
 public interface RegistrosVagaRepo extends CrudRepository<RegistrosVaga, Integer> {
 	
 	@Query(value="select CASE WHEN count(1) > 0 THEN 'true' ELSE 'false' END from registro_liberacao where id = :id", nativeQuery = true)
 	public boolean exist(int id);
+	
+	// Consulta SQL nativa para calcular a soma dos valores pagos
+    @Query(value = "SELECT SUM(valor_pago) FROM registro_liberacao", nativeQuery = true)
+    Double calcularSomaValorPago();
+
+	public List<RegistrosVaga> findByAdministrador(Administrador administradorLogado);
 	
 }
